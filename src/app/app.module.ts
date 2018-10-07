41a3dc843e5916a575d29f404dd55335c8a7ed44
Witek Podskarbi <witdev.front@gmail.com>
2018-10-07 12:42:20 +0200

    modify dependency injection and delete unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './features/demo/demo.component';
-import { ToLearnListComponent } from './features/to-learn/to-learn-list.component';
-import { ToLearnComponent } from './features/to-learn/to-learn.component';
+import { ToLearnListComponent } from './features/to-learn-list/to-learn-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    ToLearnListComponent,
-    ToLearnComponent
+    ToLearnListComponent
   ],
   imports: [
     BrowserModule
--- src/app/features/to-learn/to-learn.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-to-learn',
-  templateUrl: './to-learn.component.html'
-})
-export class ToLearnComponent { }
